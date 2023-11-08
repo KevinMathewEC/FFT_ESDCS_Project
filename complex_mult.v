d3d1ce2612f0cd4e18eff18508b5af9ab113b568
KevinMathewEC <51322169+KevinMathewEC@users.noreply.github.com>
2023-11-08 19:28:31 +0530

    Update complex_mult.v
    Right shift value changed for 1 adder

--- complex_mult.v
+++ complex_mult.v
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////
+ ///////////////////////////////////////////////////////////
 //Complex multiplier
 //////////////////////////////////////////////////////////
 `timescale 1ns / 1ps
@@ -13,9 +13,10 @@ module complex_mult(multiplicant,product);
   
   carry_look_ahead_32bit cla_32_2 (.a({multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31:4]}),.b({multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31:6]}), .carry_in(1'b0), .sum(sum2),.cout(cout2));
   
-    carry_look_ahead_32bit cla_32_3 (.a({multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31:9]}),.b({multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31:10]}), .carry_in(1'b0), .sum(sum3),.cout(cout3));
+    carry_look_ahead_32bit cla_32_3 (.a({multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31:8]}),.b({multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31],multiplicant[31:10]}), .carry_in(1'b0), .sum(sum3),.cout(cout3));
                                                                                       carry_look_ahead_32bit cla_32_4 (.a(sum1),.b(sum2), .carry_in(1'b0), .sum(sum4),.cout(cout4));
   carry_look_ahead_32bit cla_32_5 (.a(sum3),.b(sum4), .carry_in(1'b0), .sum(sum5),.cout(cout5));
   
   assign product=sum5;
+  
 endmodule
