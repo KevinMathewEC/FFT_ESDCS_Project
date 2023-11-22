0d9710c8feee9ae00e8f4bcaa29df43c92f85a14
KevinMathewEC <51322169+KevinMathewEC@users.noreply.github.com>
2023-11-22 08:31:07 +0530

    Update FFT.v

--- FFT.v
+++ FFT.v
@@ -28,7 +28,7 @@ wire c1,c2,c3,c4,c5,c6,c7;
 
 
 
-  assign b_int = (b ^ {16{carry_in}});// negate b for subtraction unless b=16'b0
+  assign b_int = (b ^ {16{carry_in}});// negate b for subtraction 
 
   carry_look_ahead_4bit cla1 (.a(a[3:0]), .b(b_int[3:0]), .cin((carry_in)), .sum(sum[3:0]), .cout(c1),.p0p1p2p3(p[0]));
 
@@ -118,7 +118,6 @@ endmodule
 
 module complex_mult(multiplicant,product);
 
-//`include "16_bit_adder.v"
 
   input signed [15:0]multiplicant;
 
@@ -140,7 +139,7 @@ module complex_mult(multiplicant,product);
 
     carry_look_ahead_16bit cla_16_3 (.a({multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15:8]}),.b({multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15],multiplicant[15:10]}), .carry_in(1'b0), .sum(sum3),.cout(cout3));
 
-                                                                                      carry_look_ahead_16bit cla_16_4 (.a(sum1),.b(sum2), .carry_in(1'b0), .sum(sum4),.cout(cout4));
+   carry_look_ahead_16bit cla_16_4 (.a(sum1),.b(sum2), .carry_in(1'b0), .sum(sum4),.cout(cout4));
 
   carry_look_ahead_16bit cla_16_5 (.a(sum3),.b(sum4), .carry_in(1'b0), .sum(sum5),.cout(cout5));
 
@@ -164,9 +163,6 @@ endmodule
 
 module BFU(A_real,A_imag,B_real,B_imag,sel_w,X0_real, X0_imag, X1_real, X1_imag);
 
-//  `include "complex_mult.v"
-
-//  `include "16_bit_adder.v"
 
   input signed[15:0] A_real,A_imag,B_real,B_imag;
 
@@ -236,17 +232,6 @@ module BFU(A_real,A_imag,B_real,B_imag,sel_w,X0_real, X0_imag, X1_real, X1_imag)
 
 
 
-//  always@(*)
-
-//    begin
-
-//     $display( "B_real_1 =%d, B_imag_1=%d ,B_real_0 =%d, B_imag_0=%d ", B_real_1,B_imag_1,B_real_0,B_imag_0);
-
-//      $display( "sum1 =%d, diff1=%d ,0.707*sum =%d, 0.707*diff=%d -b_real=%d", sum1,diff1,B_sum_prod,B_diff_prod,B_real_n);
-
-//   end
-
-  
 
    always@(*)
 
@@ -298,14 +283,6 @@ module BFU(A_real,A_imag,B_real,B_imag,sel_w,X0_real, X0_imag, X1_real, X1_imag)
 
       end
 
-//  always@(*)
-
-//   begin
-
-//     $display( "Bxw_real =%d, Bxw_imag=%d ", Bxw_real,Bxw_imag);
-
-// end
-
   
 
   carry_look_ahead_16bit cla_bfu_5 (.a(A_real),.b(Bxw_real), .carry_in(1'b0), .sum(X0_real),.cout());//A'_real
@@ -318,17 +295,6 @@ module BFU(A_real,A_imag,B_real,B_imag,sel_w,X0_real, X0_imag, X1_real, X1_imag)
 
   
 
-//  always@(*)
-
-//   begin
-
-//     $display( "A_real =%d, A_imag =%d ,A'_real =%d, A'_imag_0=%d ", A_real,A_imag,X0_real ,X0_imag);
-
-//     $display( "B'_real =%d, B'_imag=%d ", X1_real, X1_imag);
-
-//   end
-
-  
 
 endmodule
 
@@ -340,15 +306,10 @@ endmodule
 
 ///////////////////////////////////////////////////////////
 
-//`timescale 1ns / 1ps
 
 module FFT(In_real0,In_real1,In_real2,In_real3,In_real4,In_real5,In_real6,In_real7,In_imag0,In_imag1,In_imag2,In_imag3,In_imag4,In_imag5,In_imag6,In_imag7,reset_n,clk,write,start_fft,Out_real0,Out_real1,Out_real2,Out_real3,Out_real4,Out_real5,Out_real6,Out_real7,Out_imag0,Out_imag1,Out_imag2,Out_imag3,Out_imag4,Out_imag5,Out_imag6,Out_imag7,fft_ready);
 
-// `include "complex_mult.v"
 
-// `include "16_bit_adder.v"
-
-// `include "BFU.v"
 
   input signed [15:0]In_real0,In_real1,In_real2,In_real3,In_real4,In_real5,In_real6,In_real7,In_imag0,In_imag1,In_imag2,In_imag3,In_imag4,In_imag5,In_imag6,In_imag7;
 
@@ -358,13 +319,11 @@ module FFT(In_real0,In_real1,In_real2,In_real3,In_real4,In_real5,In_real6,In_rea
 
   output reg signed [15:0]Out_real0,Out_real1,Out_real2,Out_real3,Out_real4,Out_real5,Out_real6,Out_real7,Out_imag0,Out_imag1,Out_imag2,Out_imag3,Out_imag4,Out_imag5,Out_imag6,Out_imag7;
 
-  //reg signed  [15:0]  stage1_op_real[7:0],stage1_op_imag[7:0],stage2_ip_real[7:0],stage2_ip_imag[7:0],stage2_op_real[7:0],stage2_op_imag[7:0],stage3_ip_real[7:0],stage3_ip_imag[7:0];
 
   wire [15:0] stage1_op_real[7:0],stage1_op_imag[7:0],stage2_op_real[7:0],stage2_op_imag[7:0],stage3_op_real[7:0],stage3_op_imag[7:0];
 
   reg signed [15:0]stage1_ip_real[7:0],stage1_ip_imag[7:0],stage2_ip_real[7:0],stage2_ip_imag[7:0],stage3_ip_real[7:0],stage3_ip_imag[7:0],Input_real_reg[7:0],Input_imag_reg[7:0];
 
-  reg [1:0]i;//To be used to generate ready signal when output ready
   reg strt_s1,strt_s2,strt_s3;
   
   always@(posedge clk or negedge reset_n)
@@ -388,6 +347,8 @@ module FFT(In_real0,In_real1,In_real2,In_real3,In_real4,In_real5,In_real6,In_rea
       Input_imag_reg[6]<=15'd0;
       Input_imag_reg[7]<=15'b0;
       strt_s1<=1'b0;
+      strt_s2<=1'b0;
+      strt_s3<=1'b0;
       fft_ready<=1'b0; 
         end
         else if (write)
@@ -530,8 +491,6 @@ module FFT(In_real0,In_real1,In_real2,In_real3,In_real4,In_real5,In_real6,In_rea
 
   carry_look_ahead_16bit cla_fft_16 (.a(stage1_ip_imag[3]),.b(stage1_ip_imag[7]), .carry_in(1'b1), .sum(stage1_op_imag[7]),.cout());// Imaginary (x3-x7) 
 
-//  always@(*)
-//    begin
 
 
   always@(posedge clk or negedge reset_n)
@@ -808,7 +767,7 @@ module FFT(In_real0,In_real1,In_real2,In_real3,In_real4,In_real5,In_real6,In_rea
 
 	  BFU bf5 (.A_real(stage3_ip_real[3]),.A_imag(stage3_ip_imag[3]),.B_real(stage3_ip_real[7]),.B_imag(stage3_ip_imag[7]),.sel_w(2'b11),.X0_real(stage3_op_real[3])
 
-               , .X0_imag(stage3_op_imag[3]), .X1_real(stage3_op_real[7]), .X1_imag(stage3_op_imag[7])); // 11 is selected for sel_w as w^3 is the twiddle factor
+          , .X0_imag(stage3_op_imag[3]), .X1_real(stage3_op_real[7]), .X1_imag(stage3_op_imag[7])); // 11 is selected for sel_w as w^3 is the twiddle factor
 
 
 	    always@(posedge clk or negedge reset_n)
